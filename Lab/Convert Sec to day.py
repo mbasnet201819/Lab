da877d7b50f5ad462655c99790d89c72abc881b2
Mohan Basnet <mbasnet201819@gmail.com>
2021-03-10 11:56:04 +0545

    week4

--- Lab/Convert Sec to day.py
+++ Lab/Convert Sec to day.py
@@ -1,1 +1,12 @@
 ### WAP to convert sec to day, hour, minutes and seconds
+
+second = float(input("Enter the time in seconds: "))
+
+day= (((second//60)//60)//24)
+print(f"Total day for given seconds: {day}")
+
+hour= ((second//60)//60)
+print(f"Total hours for given seconds: {hour}")
+
+minute = (second)//60
+print(f"Total minute for given seconds: {minute}")
